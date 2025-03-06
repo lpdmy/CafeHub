f4f971e31d83b3e34c0172b7967e429fa96750dd
lpdmy <144099333+lpdmy@users.noreply.github.com>
2025-03-06 22:14:27 +0700

    Add entities - model

---
+++ Code/CafeHub/CafeHub.Commons/Models/Salary.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CafeHub.Commons.Models
+{
+    public class Salary
+    {
+        public int Id { get; set; }
+        public int StaffId { get; set; }
+        public decimal Amount { get; set; }
+        public string PaymentData { get; set; }
+        public Staff Staff { get; set; }
+    }
+
+}
---
+++ Code/CafeHub/CafeHub.Commons/Models/Staff.cs
@@ -0,0 +1,22 @@
+﻿using CafeHub.Commons.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CafeHub.Commons.Models
+{
+    public class Staff
+    {
+        [Key]
+        public int Id { get; set; }
+        public PositionEnum Position { get; set; }
+        public decimal FixedSalary { get; set; }
+        public SalaryTypeEnum SalaryType { get; set; }
+        public string UserId { get; set; }
+        public User User { get; set; }
+    }
+
+}
