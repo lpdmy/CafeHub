ca2f90c951b963fd1c0c88c8673a47bc15971a9f
tuấn nguyễn <tuan6620033@gmail.com>
2025-03-25 23:50:30 +0700

    feature: CRUD products by admin

--- Code/CafeHub/CafeHub.Repository/Interfaces/ICategoryRepository.cs
+++ Code/CafeHub/CafeHub.Repository/Interfaces/ICategoryRepository.cs
@@ -1,14 +1,13 @@
 ﻿using CafeHub.Commons.Models;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 
 namespace CafeHub.Repository.Interfaces
 {
     public interface ICategoryRepository : IGenericRepository<Category>
     {
+        Task<IEnumerable<Category>> GetCategoriesByNameAsync(string name);
     }
 }
-    
