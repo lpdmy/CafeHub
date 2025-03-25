5a110b85b6231bb56e7de9b5039ee6abd363e380
thienlcde170520 <thienle2105@gmail.com>
2025-03-25 21:16:54 +0700

    create display list staff

---
+++ Code/CafeHub/CafeHub.MVC/Controllers/AdminStaffManage.cs
@@ -0,0 +1,43 @@
+﻿using CafeHub.Commons.Models;
+using CafeHub.Services.Interfaces;
+using CafeHub.Web.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CafeHub.MVC.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class AdminStaffManage : Controller
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly IAccountService _accountService;
+
+        public AdminStaffManage(UserManager<User> userManager, IAccountService accountService)
+        {
+            _userManager = userManager;
+            _accountService = accountService;
+        }
+        public async Task<IActionResult> Manage_Staff()
+        {
+            var UserId = await _accountService.GetCurrentUserIdAsync();
+
+            var staffs = await _userManager.GetUsersInRoleAsync("Staff");      
+
+            var staffsList = staffs
+                 .OfType<Staff>() // Ép kiểu sang Staff
+                 .Select(s => new StaffViewModel
+                 {
+                     Id = s.Id,
+                     Name = s.Name,
+                     Email = s.Email,
+                     EmployeeCode = s.EmployeeCode,
+                     Position = s.Position,
+                     HireDate = s.HireDate,
+                     Salary = s.Salary
+                 })
+                 .ToList();
+            return View(staffsList);
+        }       
+    }
+}
