16554e365ab582d83e26bb9d062960f768213e1a
lpdmy <144099333+lpdmy@users.noreply.github.com>
2025-03-30 07:06:05 +0700

    Merge from main

--- Code/CafeHub/CafeHub.MVC/Program.cs
+++ Code/CafeHub/CafeHub.MVC/Program.cs
@@ -15,8 +15,8 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
-                        ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+var connectionString = Environment.GetEnvironmentVariable("DefaultConnection")
+    ?? builder.Configuration.GetConnectionString("DefaultConnection");
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
